a30e558b5b77a5a04675fc9476ece54d77854e8e
Adam Timberlake <adam.timberlake@gmail.com>
2014-03-05 23:01:25 +0000

    Added Nominatim for full postal code lookup. Fixes #2

--- module/php/posty.php
+++ module/php/posty.php
@@ -7,26 +7,59 @@
 class Posty {
 
     /**
-     * @constant API_URL
+     * @constant ZOOPLA_API_URL
      * @type string
      */
-    const API_URL = 'http://api.zoopla.co.uk/api/v1/property_listings.js?postcode=%s&api_key=%s';
+    const ZOOPLA_API_URL = 'http://api.zoopla.co.uk/api/v1/property_listings.js?postcode=%s&api_key=%s';
 
     /**
-     * @constant API_KEY
+     * @constant ZOOPLA_API_KEY
      * @type string
      */
-    const API_KEY = 'nrb28jhb8vydfbf7769fkat3';
+    const ZOOPLA_API_KEY = 'nrb28jhb8vydfbf7769fkat3';
+
+    /**
+     * @constant NOMINATIM_API_URL
+     * @type string
+     */
+    const NOMINATIM_API_URL = 'http://nominatim.openstreetmap.org/search?format=json&limit=5&q=%s&addressdetails=1';
 
     /**
      * @method getLatLng
      * @param string $postCode
-     * @return array
+     * @return string
      */
     public function getLatLng($postCode) {
 
+        // Remove all of the whitespace, and limit to 7 characters.
+        $postCode = str_replace(' ', '', $postCode);
+        $postCode = substr($postCode, 0, 7);
+        $latLng = null;
+
+        if (strlen($postCode) <= 4) {
+            // Use Zoopla if we only have a partial post code.
+            $latLng = $this->_fromZoopla($postCode);
+        }
+
+        if (!$latLng) {
+            // Otherwise we'll use Nominatim.
+            $latLng = $this->_fromNominatim($postCode);
+        }
+
+        return $latLng;
+
+    }
+
+    /**
+     * @method _fromZoopla
+     * @param string $postCode
+     * @return array
+     * @private
+     */
+    private function _fromZoopla($postCode) {
+
         // Fetch the data and then decode the JSON.
-        $url  = sprintf(self::API_URL, $postCode, self::API_KEY);
+        $url  = sprintf(self::ZOOPLA_API_URL, $postCode, self::ZOOPLA_API_KEY);
         $data = file_get_contents($url);
         $data = (object) json_decode($data);
 
@@ -41,8 +74,35 @@ class Posty {
 
     }
 
+    /**
+     * @method _fromNominatim
+     * @param string $postCode
+     * @return array
+     * @private
+     */
+    private function _fromNominatim($postCode) {
+
+        // Fetch the JSON data from Nominatim.
+        $url  = sprintf(self::NOMINATIM_API_URL, $this->_addSpaces($postCode));
+        $data = file_get_contents($url);
+        $data = json_decode($data);
+
+        return array('latitude' => (float) $data[0]->lat, 'longitude' => (float) $data[0]->lon);
+
+    }
+
+    /**
+     * @method _addSpaces
+     * @param string $postCode
+     * @return string
+     */
+    private function _addSpaces($postCode) {
+        $splitFrom = (strlen($postCode) === 7) ? 4 : 3;
+        return trim(chunk_split($postCode, $splitFrom, ' '));
+    }
+
 }
 
-$posty    = new Posty();
+$posty = new Posty();
 $postCode = urlencode($_GET['postCode']);
 echo json_encode($posty->getLatLng($postCode));
